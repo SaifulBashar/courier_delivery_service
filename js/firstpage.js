1e52e57447d51e98df44adac93cf590d90fbae5a
Saiful Bashar <robal.bashar59@gmail.com>
2016-11-17 22:53:05 +0600

    Second commit. check if username empty before entering password

--- js/firstpage.js
+++ js/firstpage.js
@@ -2,5 +2,20 @@
  * Created by saiful on 11/17/16.
  */
 $(function () {
-    
+    $("[name=pass]").on('click',function () {
+        if(!$("[name=uname]").val()){
+            $("[name=uname]").css({
+                "border": "3px solid red",
+
+                "background-color": "transparent"
+            });
+        }
+        else{
+            $("[name=uname]").css({
+                "border": "0px solid #000000",
+                "border-bottom-width": "3px",
+                "background-color": "transparent"
+            });
+        }
+    })
 });
