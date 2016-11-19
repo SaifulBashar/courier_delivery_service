22773695dc3a9ef6a8fb7cfe8c4c325626d4a8aa
Saiful Bashar <robal.bashar59@gmail.com>
2016-11-19 14:34:05 +0600

    Final submission of login page . Add button and keyboard interaction . Add comment . disorganized the code .

--- js/firstpage.js
+++ js/firstpage.js
@@ -2,86 +2,119 @@
  * Created by saiful on 11/17/16.
  */
 $(function () {
-    function a(data) {
+    function ajax(data) {
+        //username making empty
+        //password making empty
         $("[name=uname]").val("");
         $("#p").val("");
+        //data come from php script as json
         if (data.ok == "confirm") {
             alert("success");
+            //username making empty
+            //password making empty
             $("[name=uname]").val('');
             $("#p").val('');
+            filled_password_css();
+            filled_username_css();
         }
         else {
             alert("fail");
         }
     }
-    $("#p").on('click keypress',function (e) {
-        if(!$("[name=uname]").val()){
-            $("[name=uname]").css({
-                "border": "3px solid red",
-
-                "background-color": "transparent"
-            });
-        }
-        if($("[name=uname]").val()){
-            $("[name=uname]").css({
+    //indicate username field empty
+    function empty_username_css() {
+        $("[name=uname]").css({
+            "border": "3px solid red",
+            "background-color": "transparent"
+        });
+    }
+    //indicate password field full
+    function filled_username_css() {
+        $("[name=uname]").css({
                 "border": "0px solid #000000",
                 "border-bottom-width": "3px",
                 "background-color": "transparent"
             });
-        }
-       
+    }
+    //indicate password field empty
+    function empty_password_css() {
+        $("#p").css({
+            "border": "3px solid red",
 
+            "background-color": "transparent"
+        });
+    }
+    //indicate password field full
+    function filled_password_css() {
+        $("#p").css({
+            "border": "0px solid #000000",
+            "border-bottom-width": "3px",
+            "background-color": "transparent"
+        });
+    }
+    //check username before entering password
+    $("#p").on('click keypress',function (e) {
+        if(!$("[name=uname]").val()){
+
+            empty_username_css();
+        }
+        if($("[name=uname]").val()){
+
+            filled_username_css();
+        }
+        //add enter button event
+        if(e.keyCode ===13){
+            if(!$("#p").val() && !$("[name=uname]").val()){
+                // $("[name=uname]").css({
+                //     "border": "3px solid red",
+                //
+                //     "background-color": "transparent"
+                // });
+                // $("#p").css({
+                //     "border": "3px solid red",
+                //
+                //     "background-color": "transparent"
+                // });
+                empty_password_css();
+                empty_username_css()
+            }
+            else {
+                var form_data_name = $("[name=uname]").val();
+                var form_data_pass = $("#p").val();
+
+                $.get("login.php", {name: form_data_name, pass: form_data_pass}, ajax, "json");
+
+                filled_username_css();
+
+                filled_password_css();
+            }
+
+        }
 
     })
-
+    //login button action on click and enter button
     $("[name=login]").on("click keypress",function (e) {
         if (e.which === 13 || e.type === 'click') {
 
 
             if (!$("[name=uname]").val() && !$("#p").val()) {
-                $("[name=uname]").css({
-                    "border": "3px solid red",
-
-                    "background-color": "transparent"
-                });
-                $("#p").css({
-                    "border": "3px solid red",
-
-                    "background-color": "transparent"
-                });
+                empty_password_css();
+                empty_username_css();
             }
             else if (!$("[name=uname]").val() || !$("#p").val()) {
                 if (!$("[name=uname]").val()) {
-                    $("[name=uname]").css({
-                        "border": "3px solid red",
-
-                        "background-color": "transparent"
-                    });
+                    empty_username_css();
                 }
-                else if (!$("#p").val()) {
-                    $("#p").css({
-                        "border": "3px solid red",
-
-                        "background-color": "transparent"
-                    });
+                if (!$("#p").val()) {
+                    empty_password_css();
                 }
             }
 
             else {
                 var form_data_name = $("[name=uname]").val();
                 var form_data_pass = $("#p").val();
-                $.get("login.php", {name: form_data_name, pass: form_data_pass}, function (data) {
-                    $("[name=uname]").val("");
-                    $("#p").val("");
-                    if (data.ok == "confirm") {
-                        alert("success");
-                        $("[name=uname]").val('');
-                        $("#p").val('');
-                    }
-                    else {
-                        alert("fail");
-                    }
-                }, "json");
+                //ajax
+                $.get("login.php", {name: form_data_name, pass: form_data_pass}, ajax, "json");
 
 
             }
